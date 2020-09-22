0fe917bb83fb67ae1a24106c5421a2a186356fca
Alvin Galit <aroblesgalit@gmail.com>
2020-09-22 17:07:43 -0500

    Created class for game with just names as arguments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './pages.css'
+import Game from '../components/Game'
 
 export default function Home() {
+
+    const [game, setGame] = useState();
+
+    function handleStart() {
+        setGame(() => {
+            const newGame = new Game("Alvin", "Davis");
+            return newGame;
+        })
+    }
+
     return (
         <div>
             <h1>Tic Tac Toe</h1>
-            
+
+            <button
+                type='button'
+                className='btn btn-primary'
+                onClick={handleStart}
+            >
+                Start
+            </button>
+            <h1>
+                {
+                    game ? `${game.p1} vs ${game.p2}` : ""
+                }
+            </h1>
             <div className='container'>
                 <div className='box-wrapper row'>
                     <div className='box col-4'>O</div>
