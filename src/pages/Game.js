8c449c198ec63297a2eee007482706cdf9b0e254
Alvin Galit <aroblesgalit@gmail.com>
2020-09-21 21:31:44 -0500

    Created grid box

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './pages.css'
 
 export default function Game() {
     return (
         <div>
-            Tic Tac Toe
+            <h1>Tic Tac Toe</h1>
+            
+            <div className='container'>
+                <div className='box-wrapper row'>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                    <div className='box col-4'>O</div>
+                </div>
+            </div>
         </div>
     )
 }
