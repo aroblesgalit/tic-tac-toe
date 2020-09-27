cd8e5217e962e767a02f1ff650e1ce9a3f6beacb
Alvin Galit <aroblesgalit@gmail.com>
2020-09-27 11:39:27 -0500

    Imported board renderer class into Grid

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import './grid.css'
+import BoardRendererInterface from '../BoardRendererInterface'
 
 export default function Grid({ start, game }) {
 
     // if (game.gameOver) {
     //     return 'Start a New Game!'
     // }
+    class boardRendererInt extends BoardRendererInterface {
+        draw(marks) {
+            return (
+                marks.map((mark, i) => (
+                    <div
+                        key={i}
+                        className='box col-4'
+                        onClick={() => game.addMark(i)}
+                    >
+                        {mark}
+                    </div>
+                ))
+            )
+        }
+    }
+
+    const board = new boardRendererInt(); 
 
     return (
         <div className='box-wrapper row'>
             {
                 start ? (
-                    game.marks.map((mark, i) => (
-                        <div
-                            key={i}
-                            className='box col-4'
-                            onClick={() => game.addMark(i)}
-                        >
-                            {mark}
-                        </div>
-                    ))
+                    board.draw(game.marks)
                 ) : ''
             }
         </div>
