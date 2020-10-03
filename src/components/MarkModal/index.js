3449872036f89188e2f764c9168298729dfee168
Alvin Galit <aroblesgalit@gmail.com>
2020-10-02 23:44:12 -0500

    Centered mark modal

--- src/components/MarkModal/index.js
+++ src/components/MarkModal/index.js
@@ -3,23 +3,23 @@ import './markModal.css'
 
 export default function index({ game, closeModal }) {
     return (
-        <div className="mark-container container position-fixed">
+        <div className="mark-container position-fixed">
             <div className="row d-flex justify-content-center">
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col">
                     <div className="card">
                         <div className="card-body d-flex flex-column align-items-center">
                             <h5 className="card-title">Choose your Mark</h5>
-                            <div className="marks d-flex justify-content-around m-4">
-                                <span
+                            <div className="marks d-flex justify-content-around m-2">
+                                <div
                                     onClick={() => {game.chooseMark("o"); closeModal()}}
                                 >
                                     o
-                                </span>
-                                <span
+                                </div>
+                                <div
                                     onClick={() => {game.chooseMark("x"); closeModal()}}
                                 >
                                     x
-                                </span>
+                                </div>
                             </div>
                         </div>
                     </div>
