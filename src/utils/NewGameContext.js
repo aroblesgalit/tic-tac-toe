a952d7ac9718d283c2de4931579ac931c277992e
Alvin Galit <aroblesgalit@gmail.com>
2020-10-03 00:26:11 -0500

    Declared states

--- src/utils/NewGameContext.js
+++ src/utils/NewGameContext.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
 const NewGameContext = React.createContext();
 
 // Provider
 function NewGameProvider(props) {
+
+    const [game, setGame] = useState({
+        start: null,
+        turn: 0,
+        marks: [
+            null, null, null,
+            null, null, null,
+            null, null, null
+        ]
+    })
+
+    const [player1, setPlayer1] = useState({
+        name: '',
+        mark: null
+    })
+    
+    const [player2, setPlayer2] = useState({
+        name: '',
+        mark: null
+    })
+
     return (
         <NewGameContext.Provider>
             {props.children}
